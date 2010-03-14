e98710c2136015f06600f1ae5ef1bab98995ff68
Pentie <Pentie@bba2e408-70eb-11de-b374-81e7cb8b6ca2>
2010-03-14 13:17:08 +0000

    add: rm_pdb, remote debugger module for multiprocessing
    git-svn-id: https://ptcoding.googlecode.com/svn/trunk@14 bba2e408-70eb-11de-b374-81e7cb8b6ca2

---
+++ rm_pdb/rm_pdb.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import socket
+import sys
+import threading
+import pdb as _pdb
+
+def begin(addr = 'localhost', port = 8964):
+    def __output(conn):
+        while True:
+            data = conn.recv(1)
+            if not data:break
+            sys.stdout.write(data)
+            sys.stdout.flush()
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind((addr, port))
+    sock.listen(1)
+
+    while True:
+        conn, addr = sock.accept()
+        print "connect from %s" % str(addr)
+
+        threading.Thread(target = __output, args = (conn,)).start()
+        
+        try:
+            while 1:
+                i = sys.stdin.readline()
+                conn.sendall(i)
+        except socket.error:
+            print "Connect close."
+        finally:
+            conn.close()
+
+def pdb(addr = 'localhost', port = 8964):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((addr, port))
+
+    return _pdb.Pdb(stdin = sock.makefile('r+', 0), stdout = sock.makefile('w+', 0))
+
+
---
+++ rm_pdb/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='rm_pdb',
+      version='1.0',
+      description='A helper module for pdb to debug mutiprocessing children, over simple socket operations.',
+      author='BOYPT',
+      author_email='pentie@gmail.com',
+      py_modules=['rm_pdb'],
+      )
+
