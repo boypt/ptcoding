796827e3d0cebea93d4f291c2df26c9de4840ded
Pentie <Pentie@bba2e408-70eb-11de-b374-81e7cb8b6ca2>
2010-07-05 16:02:15 +0000

    freessh keep online
    git-svn-id: https://ptcoding.googlecode.com/svn/trunk@27 bba2e408-70eb-11de-b374-81e7cb8b6ca2

--- freessh.sh
+++ freessh.sh
@@ -8,14 +8,18 @@ auto_login_ssh () {
                 interact;";
 }
 
-HOSTS=($(curl freessh.us | sed -n "s/.*<td bgcolor='#FFFFFF'>\([0-9a-z\.]\+\?\)<\/td>.*$/\1/p"))
+conn_to_ssh () {
 
+    HOSTS=($(curl freessh.us | sed -n "s/.*<td bgcolor='#FFFFFF'>\([0-9a-z\.]\+\?\)<\/td>.*$/\1/p"))
+    RND=$(($RANDOM % 2 * 3))
+    HOST=${HOSTS[0+$RND]}
+    USR=${HOSTS[1+$RND]}
+    PSW=${HOSTS[2+$RND]}
 
-RND=$(($RANDOM % 2 * 3))
-HOST=${HOSTS[0+$RND]}
-USR=${HOSTS[1+$RND]}
-PSW=${HOSTS[2+$RND]}
+    auto_login_ssh $PSW $USR@$HOST -D 7070 -N
+}
 
-auto_login_ssh $PSW $USR@$HOST -D 7070 -N
-
+while true; do
+    conn_to_ssh
+done
 
