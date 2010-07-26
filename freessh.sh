7a5347677411a90190bd37f2cb2c39afdf5af8ba
Pentie <Pentie@bba2e408-70eb-11de-b374-81e7cb8b6ca2>
2010-07-26 06:04:00 +0000

    freessh.sh updated
    git-svn-id: https://ptcoding.googlecode.com/svn/trunk@29 bba2e408-70eb-11de-b374-81e7cb8b6ca2

--- freessh.sh
+++ freessh.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+LOCAL_PORT=7070
+
 auto_login_ssh () {
     expect -c "set timeout -1;
                 spawn ssh -o StrictHostKeyChecking=no $2 ${@:3};
@@ -9,12 +11,24 @@ auto_login_ssh () {
 }
 
 conn_to_ssh () {
-    HOSTS=($(curl freessh.us | sed -n "s/.*<td bgcolor='#FFFFFF'>\([0-9a-z\.]\+\?\)<\/td>.*$/\1/p"))
-    RND=$(($RANDOM % 2 * 3))
-    HOST=${HOSTS[0+$RND]}
-    USR=${HOSTS[1+$RND]}
-    PSW=${HOSTS[2+$RND]}
-    auto_login_ssh $PSW $USR@$HOST -D 7070 -N
+    HOSTS=($(curl -s freessh.us | sed -n "s/.*<td bgcolor='#FFFFFF'>\(.\+\?\)<\/td>.*$/\1/p"))
+    
+    for I in seq 0 7 $((${#HOSTS[@]} - 1)); do
+        if [[ ${HOSTS[$I + 6]} == "正常" ]]; then
+            IDX=$I
+            break
+        fi
+        if [[ $I -eq $((${#HOSTS[@]} - 7)) ]]; then
+            echo -e "Status: All server's busy, or something's wrong: \n\n${HOSTS[@]}"
+            exit 1
+        fi
+    done
+
+    HOST=${HOSTS[1+$IDX]}
+    USR=${HOSTS[2+$IDX]}
+    PSW=${HOSTS[3+$IDX]}
+    echo "Connecting to ${HOSTS[0+$IDX]} $HOST"
+    auto_login_ssh $PSW $USR@$HOST -NC -D $LOCAL_PORT
 }
 
 while true; do
