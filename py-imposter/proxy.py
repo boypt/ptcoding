a5b847268a3c6512e310a449b1aa2ddeca45e7eb
Preston <Pentie@gmail.com>
2012-06-10 22:04:06 +0800

    curl work with headers

--- py-imposter/proxy.py
+++ py-imposter/proxy.py
@@ -1,52 +1,67 @@
 #!/usr/bin/env python2
 
 import bottle
-from bottle import request, post, redirect, error, get
+from bottle import request, response, HTTPResponse, post, redirect, error, get
 from cStringIO import StringIO
 import pycurl
+import re
+from wsgiref.util import is_hop_by_hop
 
-bottle.debug(1)
+STRIP_HEADERS = frozenset(("host", "via", "x-forwarded-for"))
+
+bottle.debug()
 
 class CurlNon200Status(Exception):
     pass
 
 def curl_http(uri, headers):
 
-    buf = StringIO()
+    headerbuf = StringIO()
+    bodybuf = StringIO()
     c = pycurl.Curl()
     try:
         c.setopt(c.URL, uri)
-        c.setopt(c.CONNECTTIMEOUT, 5)
-        c.setopt(c.TIMEOUT, 8)
+        c.setopt(c.CONNECTTIMEOUT, 30)
+        c.setopt(c.TIMEOUT, 30)
         c.setopt(c.FAILONERROR, True)
 
         c.setopt(c.HTTPHEADER, headers)
-        c.setopt(pycurl.WRITEFUNCTION, buf.write)
+        c.setopt(pycurl.HEADERFUNCTION, headerbuf.write)
+        c.setopt(pycurl.WRITEFUNCTION, bodybuf.write)
         c.perform()
 
         if c.getinfo(pycurl.HTTP_CODE) != 200:
             raise CurlNon200Status()
 
-        content = buf.getvalue()
+        headers = headerbuf.getvalue()
+        content = bodybuf.getvalue()
+
+        headtuple = re.findall(r"(?P<name>.*?): (?P<value>.*?)\r\n", headers)
 
     finally:
         c.close()
-        buf.close()
+        headerbuf.close()
+        bodybuf.close()
 
-    return content
+    return headtuple, content
 
 
 @get('/proxy')
 def proxy():
     url = request.GET.get("url")
-    headers = ["{0}: {1}".format(k,v) for k,v in request.headers.iteritems()]
-    import ipdb; ipdb.set_trace()
+    headers = ["{0}: {1}".format(k,v) for k,v in request.headers.iteritems() if k.lower() not in STRIP_HEADERS]
 
     try:
-        content = curl_http(url, headers)
+        headtuple, content = curl_http(url, headers)
+#        import ipdb;ipdb.set_trace()    
+        headret = [(k,v) for k,v in headtuple if not is_hop_by_hop(k)]
+        raise HTTPResponse(output = content, header = headret)
+    except (pycurl.error, CurlNon200Status):
+        redirect(url)
+
     except Exception, e:
         raise
 
 
-bottle.run(bottle.app())
+bottle.run(debug = True)
 
