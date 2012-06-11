20683a38014316c12c698c0a1445eaa9efea11fc
Preston M <pentie@gmail.com>
2012-06-11 15:18:47 +0800

    memcached status check

--- py-imposter/proxy.py
+++ py-imposter/proxy.py
@@ -5,11 +5,36 @@ from bottle import request, response, HTTPResponse, post, redirect, error, get
 from cStringIO import StringIO
 import pycurl
 import re
+import hashlib
+import logging
 from wsgiref.util import is_hop_by_hop
+
+import pylibmc
+mc = pylibmc.Client(["127.0.0.1"], binary=True,
+                        behaviors={"tcp_nodelay": True,
+                        "ketama": True})
+
+markurl = lambda u:mc.set(hashlib.md5(u).hexdigest(), 1, time = 3600)
+
+def log_init(log_file, quiet = False, name = __name__):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    hdlr = logging.FileHandler(log_file)
+    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+    hdlr.setFormatter(formatter)
+    logger.addHandler(hdlr)
+    if not quiet:
+        hdlr = logging.StreamHandler()
+        hdlr.setFormatter(formatter)
+        logger.addHandler(hdlr)
+    return logger
+
+log = log_init("/tmp/proxy.log")
 
 STRIP_HEADERS = frozenset(("host", "via", "x-forwarded-for"))
 
-bottle.debug()
+
+
 
 class CurlNon200Status(Exception):
     pass
@@ -21,8 +46,8 @@ def curl_http(uri, headers):
     c = pycurl.Curl()
     try:
         c.setopt(c.URL, uri)
-        c.setopt(c.CONNECTTIMEOUT, 30)
-        c.setopt(c.TIMEOUT, 30)
+        c.setopt(c.CONNECTTIMEOUT, 10)
+        c.setopt(c.TIMEOUT, 10)
         c.setopt(c.FAILONERROR, True)
 
         c.setopt(c.HTTPHEADER, headers)
@@ -32,6 +57,10 @@ def curl_http(uri, headers):
 
         if c.getinfo(pycurl.HTTP_CODE) != 200:
             raise CurlNon200Status()
+
+        if not c.getinfo(pycurl.CONTENT_TYPE).startswith("text/html"):
+            log.debug("non-html: " + uri)
+            markurl(uri)
 
         headers = headerbuf.getvalue()
         content = bodybuf.getvalue()
@@ -56,12 +85,17 @@ def proxy():
 #        import ipdb;ipdb.set_trace()    
         headret = [(k,v) for k,v in headtuple if not is_hop_by_hop(k)]
         raise HTTPResponse(output = content, header = headret)
-    except (pycurl.error, CurlNon200Status):
+    except pycurl.error, e:
+        log.debug("curl error: " + str(e))
+        markurl(url)
         redirect(url)
-
+    except CurlNon200Status:
+        log.debug("non 200: ")
+        markurl(url)
+        redirect(url)
     except Exception, e:
         raise
 
 
-bottle.run(debug = True)
+bottle.run(host = "0.0.0.0", debug = True)
 
