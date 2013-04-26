12194d2ac82669700e1150483e4f10a6e324bd29
Preston Mason <pentie@gmail.com>
2013-04-26 11:29:05 +0800

    update screenshoot for new zenity returns

--- bash/screenshoot.sh
+++ bash/screenshoot.sh
@@ -11,11 +11,11 @@ fi
 
 ACTION=$(zenity --list  --height=250 --title="What to do with the image" --column="How" "Save" "Temp" "Upload")
 
-if [[ $ACTION == "Save" ]]; then
+if [[ $ACTION == "Save"* ]]; then
     TO_PATH=$(zenity --file-selection --save --confirm-overwrite --filename "$SCREEN")
     mv "$SCREEN" "$TO_PATH"
     zenity --info --text="$TO_PATH"
-elif [[ $ACTION == "Upload" ]]; then
+elif [[ $ACTION == "Upload"* ]]; then
     #URL=$(curl -F "image=@$SCREEN" -F "key=486690f872c678126a2c09a9e196ce1b" http://imgur.com/api/upload.xml|sed -n "s/^.\+<original_image>\(.\+\)<\/original_image>.\+/\1/p")
     URL=$(curl -F "name=@$SCREEN" http://img.vim-cn.com/)
     zenity --info --text="$URL"
