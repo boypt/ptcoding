9c46785f7e9a984b1e8c090b0ca222ab77974059
Preston Mason <pentie@gmail.com>
2013-05-09 10:52:05 +0800

    php filename

--- php/coded_filename.php
+++ php/coded_filename.php
@@ -1,34 +1,35 @@
 <?php
-require_once('Log.php');
-//$logger = Log::factory('syslog', '', 'APC-Checker');
-$logger = Log::factory('error_log', PEAR_LOG_TYPE_SYSTEM, 'Coded');
 
-$ROOT = "/srv/http/public_html/appgame";
+$root = dirname(__FILE__);
+$req = $_SERVER["REQUEST_URI"];
 
-$fn = $_SERVER["REQUEST_URI"];
+error_log("Requested: ".$_SERVER['HTTP_HOST'].$req);
 
-$logger->log("Requested: ".$_SERVER['HTTP_HOST'].$fn);
-
-if(mb_detect_encoding($fn, 'ASCII', true)) {
-    $logger->log("pure ascii, may urlencoded.");
-    $fn = urldecode($fn);
+if(strpos($req, "/wp-content/uploads/") !== 0) {
+    error_log("Non-content request. Forbided.");
+    header("HTTP/1.1 403 Forbbiden.");
+    die();
 }
 
-
-$utf8_path = @mb_convert_encoding($fn, "utf-8", array("cp936", "gbk", "big5"));
-
-if($utf8_path === null || !is_file($ROOT . $utf8_path)) {
-    $logger->log("can't decode, try forced decode utf8.");
-    $utf8_path = @utf8_decode($fn);
+if(mb_detect_encoding($req, 'ASCII', true)) {
+    error_log("Pure ascii, try urlencoded.");
+    $req = rawurldecode($req);
 }
 
-if($utf8_path !== null && is_file($ROOT . $utf8_path)) {
-    $logger->log("File Found: $utf8_path");
-    header("Content-Type: ". mime_content_type($ROOT . $utf8_path));
+$utf8_path = @mb_convert_encoding($req, "utf-8", array("cp936", "gbk", "big5"));
+
+if($utf8_path === null || !is_file($root . $utf8_path)) {
+    error_log("can't decode, try forced decode utf8.");
+    $utf8_path = @utf8_decode($req);
+}
+
+if($utf8_path !== null && is_file($root . $utf8_path)) {
+    error_log("File Found: $utf8_path");
+    header("Content-Type: ". mime_content_type($root . $utf8_path));
     header("X-Accel-Redirect: " . $utf8_path);
 } else {
-    $logger->log("can't decode, not found");
-    header("HTTP/1.0 404 Not Found");
+    error_log("can't decode, not found");
+    header("HTTP/1.1 404 Not Found");
     die();
 }
 ?>
