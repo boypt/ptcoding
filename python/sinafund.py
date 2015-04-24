59713b3bb3fa386a75a8bc1c33295dfe6ad4cd4d
Preston Mason <pentie@gmail.com>
2015-04-24 17:24:21 +0800

    fund/stock refactor

--- python/sinafund.py
+++ python/sinafund.py
@@ -7,16 +7,30 @@ import urllib.request, urllib.error, urllib.parse
 
 
 def sina_fund(num):
-    req = urllib.request.Request("http://hq.sinajs.cn/list=%s" % num)
+    query_str = ",".join(map(lambda z:'f_{}'.format(z), num))
+    req = urllib.request.Request("http://hq.sinajs.cn/list="+query_str)
     data = urllib.request.urlopen(req).read().decode('gbk')
-    reg = re.compile(r'var hq_str_f_\d{6}="(.+?)";\n')
-    #with open('/tmp/st.txt', 'rb') as f: data = f.read().decode('gbk')
-    stval = reg.findall(data)
-    stall = tuple(map(lambda q:tuple(q.split(',')), stval))
+    reg = re.compile(r'var hq_str_f_(\d{6})="(.+?)";\n')
+    stall = reg.findall(data)
 
     return stall
 
 
+def print_fund(fund_all):
+
+    for num,fund in fund_all:
+        q=fund.split(',')
+        name = q[0]
+        curval = float(q[1])
+        lastval = float(q[3])
+        date = q[4]
+        incr = (curval-lastval)/lastval
+
+        s_curval = "{:.4f}".format(curval).rjust(7)
+        s_lastval = "{:.4f}".format(lastval).rjust(7)
+        s_incr = "{:+.2%}".format(incr).rjust(7)
+
+        print("{}  {}  {}   {} -- {}{}".format(s_curval,s_incr,s_lastval,date,num,name))
 
 
 if __name__ == '__main__':
@@ -24,29 +38,14 @@ if __name__ == '__main__':
     num_fn = sys.argv[1]
 
     with open(num_fn, 'rb') as f:
-        nums = f.read()
-        num_re = re.compile(b"\d{6}")
+        nums = f.read().decode('ascii', 'ignore')
+        num_re = re.compile(r"\d{6}")
         file_num = num_re.findall(nums)
-        item_num = ",".join(map(lambda z:'f_'+z.decode('ascii'), file_num))
-        stall = sina_fund(item_num)
+        stall = sina_fund(file_num)
 
-        print("--净值-----涨幅-----昨净-----净值日期-----------基金全称-----------")
-
-        for q in stall:
-            #for n,v in enumerate(q): print (n,v)
-            name = q[0]
-            curval = float(q[1])
-            lastval = float(q[3])
-            date = q[4]
-            incr = (curval-lastval)/lastval
-
-            s_curval = "{:.4f}".format(curval).rjust(7)
-            s_lastval = "{:.4f}".format(lastval).rjust(7)
-            s_incr = "{:+.2%}".format(incr).rjust(7)
-
-            print("{0}  {1}  {2}   {3} -- {4}".format(s_curval,s_incr,s_lastval,date,name))
-            
+        print("--净值-----涨幅-----昨净-----净值日期-----------基金全称--------------")
+        print_fund(stall)
         print("-"*70)
         if os.name == 'nt':
             os.system("pause")
-        
+
--- python/sinastock.py
+++ python/sinastock.py
@@ -5,20 +5,7 @@ import sys
 import os
 import urllib.request, urllib.error, urllib.parse
 
-reg = re.compile(r'var hq_str_s_[szh]{2}\d{6}="(.+?)";\n')
-
-def a_stock(num):
-    req = urllib.request.Request("http://hq.sinajs.cn/list=%s" % num)
-    data = urllib.request.urlopen(req).read().decode('gbk')
-    #with open('/tmp/st.txt', 'rb') as f: data = f.read().decode('gbk')
-    
-    stval = reg.findall(data)
-    stall = tuple(map(lambda q:tuple(q.split(',')), stval))
-
-    return stall
-    
-def market(num):
-    markets = {
+MARKET_PREFIX = {
         "6":"sh",
         "9":"sh",
         "5":"sh",
@@ -28,39 +15,50 @@ def market(num):
         "1":"sz",
     }
 
-    return "s_{0}{1}".format(markets[num[0]], num)
+def sina_stock(all_num):
+    reg = re.compile(r'var hq_str_s_[szh]{2}(\d{6})="(.+?)";\n')
+    market_num = map(lambda num: "s_{}{}".format(MARKET_PREFIX[num[0]], num), all_num)
+    query_str = ",".join(market_num)
+    req = urllib.request.Request("http://hq.sinajs.cn/list="+query_str)
+    data = urllib.request.urlopen(req).read().decode('gbk')
+    stall = reg.findall(data)
+
+    return stall
+    
+
+def print_stock(stock_list):
+
+    for num, stock in stock_list:
+
+        q = stock.split(',')
+
+        # for n,v in enumerate(q): print (n,v)
+        name = q[0]
+        curval = float(q[1])
+        incr = float(q[2])
+        incr_pct = float(q[3])
+
+        if curval == 0:
+            curval=1
+
+        curval_str = "{:.4f}".format(curval).rjust(8)
+        incr_str = "{:+.4f}".format(incr).rjust(7)
+        incr_pct_str = "{:+.2f}%".format(incr_pct).rjust(7)
+        print("{}  {}  {}     {}{}".format(curval_str,incr_str,incr_pct_str,num,name))
 
 if __name__ == '__main__':
 
     num_fn = sys.argv[1]
 
     with open(num_fn, 'rb') as f:
-        nums = f.read()
-        num_re = re.compile(b"\d{6}")
-
+        nums = f.read().decode('ascii', 'ignore')
+        num_re = re.compile(r"\d{6}")
         file_num = num_re.findall(nums)
-        all_num = map(lambda z:z.decode('ascii'), file_num)
-        market_num = map(market, all_num)
-        stock_num = ",".join(market_num)
-        # print(stock_num)
-        stall = a_stock(stock_num)
+        stall = sina_stock(file_num)
 
         print("---最新-----涨跌-----涨幅------名称-----------------------------------")
-        for q in stall:
-            # for n,v in enumerate(q): print (n,v)
-            name = q[0]
-            curval = float(q[1])
-            incr = float(q[2])
-            incr_pct = float(q[3])
-
-            if curval == 0:
-                curval=1
-
-            curval_str = "{:.4f}".format(curval).rjust(8)
-            incr_str = "{:+.4f}".format(incr).rjust(7)
-            incr_pct_str = "{:+.2f}%".format(incr_pct).rjust(7)
-            print("{0}  {1}  {2}     {3}".format(curval_str,incr_str,incr_pct_str,name))
-
+        print_stock(stall)
         print("-"*70)
         if os.name == 'nt':
             os.system("pause")
+
