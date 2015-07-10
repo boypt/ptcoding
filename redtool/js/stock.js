74d07471e94fd6d35144811757d1c4012772d73a
Preston Mason <pentie@gmail.com>
2015-07-10 22:29:43 +0800

    disable autowidth

--- redtool/js/stock.js
+++ redtool/js/stock.js
@@ -145,6 +145,7 @@ Portfolio.prototype.init_data_table = function () {
             "info":     false,
             "searching":false,
             "deferRender": true,
+            "autoWidth": false,
             "columns": colms,
             "drawCallback":drawcb
         });
