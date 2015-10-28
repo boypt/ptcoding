cca6aaddd83de17ba23c93acbcf63619b0bd904d
Preston Mason <pentie@gmail.com>
2015-10-28 16:25:44 +0800

    add xware start script for openwrt

---
+++ bash/xware_start.sh
@@ -0,0 +1,59 @@
+#./etm_xware --system_path=. --disk_cfg=./thunder_mounts.cfg --etm_cfg=./etm.ini --log_cfg=./log.ini --pid_file=/tmp/etm.pid --license=1508062001000004u0000007vub32aatrnhgtpuwdz --import_v1v2_mode=2 --listen_addr=0.0.0.0:19000 --hubble_report_pipe_path=/tmp/etm.pipe
+
+
+XWARE_DIR=/root/xware
+XWARE=$XWARE_DIR/etm_xware
+MOUNTS=$XWARE_DIR/thunder_mounts.cfg
+ETMINI=$XWARE_DIR/etm.ini
+LOGINI=$XWARE_DIR/log.ini
+HUBBLE_PIPE=/tmp/hub.pipe
+PID=/tmp/etm.pid
+LICENSE=1508062001000004u0000007vub32aatrnhgtpuwdz
+LISTEN=0.0.0.0:19000
+ETM_URL=http://127.0.0.1:19000/getsysinfo
+
+xware_start () {
+( ${XWARE} --system_path="$XWARE_DIR" --disk_cfg="$MOUNTS" --etm_cfg="$ETMINI" --log_cfg="$LOGINI" --pid_file="$PID" --license="$LICENSE" --import_v1v2_mode=2 --listen_addr="$LISTEN" --hubble_report_pipe_path="$HUBBLE_PIPE" | logger & )
+}
+
+
+xware_stop () {
+    kill `cat $PID`
+}
+
+xware_status () {
+    if kill -0 `cat $PID` > /dev/null 2>&1; then
+        echo "xware running"
+    else
+        echo "xware dead"
+    fi
+}
+
+xware_check () {
+    if ! wget -q -s http://127.0.0.1:19000/getsysinfo > /dev/null 2>&1; then
+        xware_start
+    fi
+}
+
+case "$1" in
+        start)
+            xware_start
+            ;;
+        stop)
+            xware_stop
+            ;;
+        status)
+            xware_status
+            ;;
+        restart)
+            xware_stop
+            xware_start
+            ;;
+        check)
+            xware_check
+            ;;
+        *)
+            echo $"Usage: $0 {start|stop|restart|condrestart|status}"
+            exit 1
+esac
+
