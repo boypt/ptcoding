276e564c5e8f4fa1c7fe4c419411fd72fad97b67
PrestonM <pentie@gmail.com>
2016-01-01 22:08:28 +0800

    use curl instead

--- bash/klouderr.sh
+++ bash/klouderr.sh
@@ -7,14 +7,33 @@ if ! echo $URL | grep -q 'klouderr.com/download.php'; then
 fi
 
 TEMPIMG=$(mktemp)
+TEMPCOOKIE=$(mktemp)
 CAPTCHA=http://klouderr.com/captcha.php?rand=0.${RANDOM}${RANDOM}${RANDOM}
+CURL="curl -v --silent --cookie $TEMPCOOKIE --cookie-jar $TEMPCOOKIE --user-agent 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36' --referer $URL"
 echo "Getting: $URL"
 
-http $URL  --session cap1  > /dev/null
-http $CAPTCHA Referer:$URL  --session cap1  > $TEMPIMG
-mplayer  -monitorpixelaspect 0.5 -nosub -contrast 25 -framedrop  -vo caca -quiet $TEMPIMG   2>/dev/null
+#http $URL  --session cap1  > /dev/null
+
+eval "$CURL $URL" > /dev/null
+#http $CAPTCHA Referer:$URL  --session cap1  > $TEMPIMG
+
+eval "$CURL $CAPTCHA > $TEMPIMG"
+mplayer -monitorpixelaspect 0.5 -nosub -contrast 25 -framedrop  -vo caca -quiet $TEMPIMG   2>/dev/null
 echo -n "Enter Code:"
 read CODE
-http $URL downloadverify=1 d=1 captchacode=$CODE  --session cap1 --form -p b | grep -oE "http://.+?torrent"
 
-rm $TEMPIMG
+#http $URL downloadverify=1 d=1 captchacode=$CODE  --session cap1 --form -p b | grep -oE "http://.+?torrent"
+TORRENT=$(eval "$CURL -d 'downloadverify=1&d=1&captchacode='$CODE $URL" | grep -oE "http://.+?torrent" | head -n 1)
+
+echo "-------------------------------------------"
+echo "$TORRENT";
+echo "-------------------------------------------"
+
+#FILENAME=$(urldecode  $TORRENT | grep -oE '[^=]+?.torrent')
+#echo "$CURL -o '/tmp/$FILENAME' '$TORRENT'"
+#eval "$CURL -o '/tmp/$FILENAME' '$TORRENT'"
+echo wget --content-disposition \'"$TORRENT"\'
+wget --content-disposition "$TORRENT"
+
+echo "Downloaded: $FILENAME"
+rm $TEMPIMG $TEMPCOOKIE
