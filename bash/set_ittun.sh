d97b20fa839a18aaa7c390e60ab5b2522c562966
PrestonM <pentie@gmail.com>
2017-04-13 15:15:03 +0800

    fix: read port in loop

--- bash/set_ittun.sh
+++ bash/set_ittun.sh
@@ -6,6 +6,29 @@ set -o pipefail
 set -o nounset
 # set -o xtrace
 
+
+setconfig () {
+    while true; do
+        echo "Enter mapping port number: (must be greater than 50000)"
+        read PORT
+        if echo $PORT | grep -q -E "^5[0-9]{4}$"; then
+            break;
+        fi
+    done
+
+    cat > /etc/ngrok.yml << EOF
+    server_addr: "ittun.com:44433"
+    tunnels: 
+    ssh:
+    remote_port: $PORT
+    proto:
+    tcp: ":22"
+EOF
+
+    cat /etc/ngrok.yml 
+    echo "---------------------"
+}
+
 ARCH=$(uname -m)
 ITTUN_URL=http://www.ittun.com/upload/17.4/
 TEMPDIR=$(mktemp -d)
@@ -19,29 +42,19 @@ else
     exit 1
 fi
 
+setconfig
+
+echo "Selected $FILE";
 URL=${ITTUN_URL}${FILE}
 cd $TEMPDIR
 wget $URL
 unzip $FILE
 UNDIR=${FILE%.*}
 install -v -m755 ./$UNDIR/ngrok /usr/local/bin/
-rm -rf $TEMPDIR
+cd -
+rm -rfv $TEMPDIR
 
-echo "Enter mapping port: (must be greater than 51000)"
-read PORT
-
-cat > /etc/ngrok.yml << EOF
-server_addr: "ittun.com:44433"
-tunnels: 
-    ssh:
-       remote_port: $PORT
-       proto:
-            tcp: ":22"
-EOF
-
-cat /etc/ngrok.yml 
-echo "---------------------"
-sed -e -i '$i \ngrok -log=stdout -config=/etc/ngrok.yml start ssh >/dev/null 2>&1 &\n' rc.local
+sed -i -e '$i \ngrok -log=stdout -config=/etc/ngrok.yml start ssh >/dev/null 2>&1 &\n' rc.local
 echo "---------------------"
 cat /etc/rc.local
 echo "Done"
