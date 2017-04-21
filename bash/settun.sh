cecf4c6e54a5a7c33175c52f41af598c3f33edd4
PrestonM <pentie@gmail.com>
2017-04-21 17:49:07 +0800

    add server token

--- bash/settun.sh
+++ bash/settun.sh
@@ -6,8 +6,9 @@ set -o pipefail
 set -o nounset
 # set -o xtrace
 
-ngrok_CLIENT=http://wxdev.doctorcom.com/ngrok/ngrok-$(uname -m).tar.gz
-ngrok_SERVER=wx.ptsang.net:44443
+NGROK_CLIENT=http://wxdev.doctorcom.com/ngrok/ngrok-$(uname -m).tar.gz
+NGROK_SERVER=wx.ptsang.net:44443
+NGROK_TOKEN=
 
 PORT="${1:-}"
 setconfig () {
@@ -27,10 +28,19 @@ setconfig () {
             fi
         done
     fi
+
+    echo "Enter ServerToken"
+    if ! tty --silent; then
+        read NGROK_TOKEN < /dev/tty
+    else
+        read NGROK_TOKEN
+    fi
+
     echo "Mapping remote port $PORT"
 
     cat > $CONFIG << EOF
-server_addr: "$ngrok_SERVER"
+server_addr: "$NGROK_SERVER"
+auth_token: "$NGROK_TOKEN"
 tunnels:
     ssh:
         remote_port: $PORT
@@ -51,20 +61,20 @@ else
 fi
 
 
-ngrok_CLIENT_TAR=$(basename $ngrok_CLIENT)
+NGROK_CLIENT_TAR=$(basename $NGROK_CLIENT)
 cd /tmp
 
-$DOWNLOAD ${ngrok_CLIENT}.md5
-if ! md5sum -c $(basename ${ngrok_CLIENT}.md5); then
-    $DOWNLOAD $ngrok_CLIENT
-    md5sum -c $(basename ${ngrok_CLIENT}.md5)
+$DOWNLOAD ${NGROK_CLIENT}.md5
+if ! md5sum -c $(basename ${NGROK_CLIENT}.md5); then
+    $DOWNLOAD $NGROK_CLIENT
+    md5sum -c $(basename ${NGROK_CLIENT}.md5)
 fi
 
 
 setconfig /tmp/ngrok.yml
 if [[ $(id -u) == 0 ]]; then
   #is root
-  tar xfz /tmp/$ngrok_CLIENT_TAR -C /usr/local/bin/
+  tar xfz /tmp/$NGROK_CLIENT_TAR -C /usr/local/bin/
   install -v -m644 /tmp/ngrok.yml /etc/ngrok.yml
   sed -i -e '$i \ngrok -log=stdout -config=/etc/ngrok.yml start ssh >/dev/null 2>&1 &\n' /etc/rc.local
   echo "ngrok installed as root at /usr/local/bin/ngrok"
@@ -72,12 +82,12 @@ if [[ $(id -u) == 0 ]]; then
   echo "Autorun ngrok configured at /etc/rc.local"
   ngrok -log=stdout -config=/etc/ngrok.yml start ssh >/dev/null 2>&1 &
 else
-  tar xvfz /tmp/$ngrok_CLIENT_TAR -C ~
+  tar xvfz /tmp/$NGROK_CLIENT_TAR -C ~
   install -v -m644  /tmp/ngrok.yml ~/.ngrok
   echo "ngrok installed at $HOME/ngrok"
   echo "Config: $HOME/.ngrok"
   echo "RUN: "
-  echo "~/ngrok -config=$HOME/.ngrok start ssh"
+  echo "~/ngrok start ssh"
 fi
 rm -f /tmp/ngrok.yml
 
