22ced0ae7529a3557731c1e864a8f5a8e49b5788
PrestonM <pentie@gmail.com>
2017-07-24 10:27:02 +0800

    idx updt

--- stock-tools/index2serverchan.py
+++ stock-tools/index2serverchan.py
@@ -1,43 +1,38 @@
-#!/usr/bin/python3
-
+#!/usr/bin/env python3.6
 import re
 import sys
 import os
 import json
 import urllib.request, urllib.error, urllib.parse
 
-
 def sina_stock(all_num):
-    reg = re.compile(r'var hq_str_(.+)="(.+?)";\n')
-
+    reg = re.compile(r'var hq_str_(.+?)="(.+?)";\n')
     query_str = ",".join(all_num)
     url = "http://hq.sinajs.cn/list="+query_str
     req = urllib.request.Request(url)
     data = urllib.request.urlopen(req).read().decode('gbk')
     stall = reg.findall(data)
-
     return stall
 
 def noti_serverchan(title, desp):
     url = "https://pushbear.ftqq.com/sub"
     data = {
-	"sendkey": "522-f933512d43abe659548998b284ceec89",
-	"text": title,
-	"desp": desp
+    	"sendkey": "522-f933512d43abe659548998b284ceec89",
+    	"text": title,
+    	"desp": desp
     }
     req = urllib.request.Request(url, data = urllib.parse.urlencode(data).encode('utf-8'), method="POST")
     ret = urllib.request.urlopen(req).read()
     return json.loads(ret.decode())
-    
+
 
 def format_stockindex():
-
 
     #indexs = ['sh000001','sz399001','hkHSI','gb_$dji','hkHSI_i']
     indexs = ['sh000001','sz399001']
     vals = sina_stock(indexs)
-    content = ""
-    title = ""
+    content = []
+    title = []
     for num, inx in vals:
         q = inx.split(',')
 #"上证指数,3236.5881,3244.8647,3237.9817,3247.7122,3231.9556,0,0,206003251,221956909910,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2017-07-21,15:01:03,00";
@@ -54,8 +49,8 @@ def format_stockindex():
         else:
             changechn = '平'
 
-        title += "{0}{1}{2:+.2f}　".format(name[:2], changechn, incr_pct)
-        content += "{0}{1:.2f}".format(name, curval)
+        title.append((name, changechn, incr_pct))
+        content.append((name, curval, incr_pct))
 
     return title, content
 
@@ -65,9 +60,6 @@ if __name__ == '__main__':
      #val = sina_stock()
      #print(val)
      title, content = format_stockindex()
-     print(title)
-     noti_serverchan(title, content)
-
-
-
-
+     tit = [ "{0}{1}{2:.2f}".format(*val) for val in title ]
+     desp = ["## {0} {1:.2f} {2:+.2f}".format(*val) for val in content]
+     noti_serverchan('__'.join(tit), '\n'.join(desp))
