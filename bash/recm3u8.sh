f7b4ced006fee0dc86f4959997e7fcf3812f0205
ubuntu <ubuntu@localhost.localdomain>
2018-07-31 09:57:51 +0800

    to opus

--- bash/recm3u8.sh
+++ bash/recm3u8.sh
@@ -15,9 +15,10 @@ __root="$(cd "$(dirname "${__dir}")" && pwd)" # <-- change this as it depends on
 arg1="${1:-}"
 
 RECPATH=/srv/md/mp4
-RECNAME=${RECPATH}/gz-$(date '+%m%d-%H%M%S').mp4
-FFMPEGCMD="/usr/local/bin/ffmpeg -hide_banner -loglevel panic -re -i http://satellitepull.cnr.cn/live/wxgdgsgb/playlist.m3u8  -c copy -bsf:a aac_adtstoasc"
+RECNAME=${RECPATH}/gz-$(date '+%m%d-%H%M%S')
+#FFMPEGCMD="/usr/local/bin/ffmpeg -hide_banner -loglevel panic -re -i http://satellitepull.cnr.cn/live/wxgdgsgb/playlist.m3u8  -c copy -bsf:a aac_adtstoasc"
+FFMPEGCMD="/usr/local/bin/ffmpeg -hide_banner -loglevel panic -re -i http://satellitepull.cnr.cn/live/wxgdgsgb/playlist.m3u8 -c:a libopus -ab 64k -af pan=mono|c0=.5*c0+.5*c1 ${RECNAME}.opus"
 
 killall -q ffmpeg || true
-${FFMPEGCMD} $RECNAME &
+${FFMPEGCMD} &
 
