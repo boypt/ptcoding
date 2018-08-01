498198aa0449eac4e632e775e1d2038f20f989f1
ubuntu <ubuntu@localhost.localdomain>
2018-08-01 10:54:05 +0800

    comps

--- bash/recm3u8.sh
+++ bash/recm3u8.sh
@@ -14,10 +14,12 @@ __root="$(cd "$(dirname "${__dir}")" && pwd)" # <-- change this as it depends on
 
 arg1="${1:-}"
 
+#SRCINPUT="http://satellitepull.cnr.cn/live/wxgdgsgb/playlist.m3u8"
+SRCINPUT="http://ls.qingting.fm/live/4847/64k.m3u8?deviceid=00000000-2a50-2e6a-7cbf-0d991c12983e"
 RECPATH=/srv/md/mp4
-RECNAME=${RECPATH}/gz-$(date '+%m%d-%H%M%S')
-#FFMPEGCMD="/usr/local/bin/ffmpeg -hide_banner -loglevel panic -re -i http://satellitepull.cnr.cn/live/wxgdgsgb/playlist.m3u8  -c copy -bsf:a aac_adtstoasc"
-FFMPEGCMD="/usr/local/bin/ffmpeg -hide_banner -loglevel panic -re -i http://satellitepull.cnr.cn/live/wxgdgsgb/playlist.m3u8 -c:a libopus -ab 64k -af pan=mono|c0=.5*c0+.5*c1 ${RECNAME}.opus"
+RECNAME=${RECPATH}/$(date '+%m%d-%H%M%S')
+#FFMPEGCMD="/usr/local/bin/ffmpeg -hide_banner -loglevel panic -re -i ${SRCINPUT} -c copy -bsf:a aac_adtstoasc ${RECNAME}.opus"
+FFMPEGCMD="/usr/local/bin/ffmpeg -hide_banner -loglevel panic -re -i ${SRCINPUT} -c:a libopus -ab 12k -af pan=mono|c0=.5*c0+.5*c1 ${RECNAME}.opus"
 
 killall -q ffmpeg || true
 ${FFMPEGCMD} &
