fbf2b85c8c6713d1c9ad16ffd161e3f601f73409
boypt <pentie@gmail.com>
2018-11-13 14:23:49 +0800

    improved torrent2cloud usage

--- python/torrent2cloud.py
+++ python/torrent2cloud.py
@@ -12,21 +12,32 @@ import os
 import re
 import glob
 import logging
-
+import urllib
+import argparse
 from http.client import HTTPConnection
-# HTTPConnection.debuglevel = 1
-logging.basicConfig() # 初始化 logging，否则不会看到任何 requests 的输出。
-logging.getLogger().setLevel(logging.DEBUG)
-requests_log = logging.getLogger("requests.packages.urllib3")
-requests_log.setLevel(logging.DEBUG)
 
-aria2_url=""
-aria2_token=""
-sourceroot=""
 CLDTORRENT=""
 CLDCOOKIE=""
-
 torrent_local_path="/mnt/d/*.torrent"
+
+def readconf():
+    conf = os.path.expanduser("~/.ptutils.config")
+    if os.path.exists(conf):
+        with open(conf) as f:
+            configs = f.readlines()
+        
+        glbs = globals()
+        for f in configs:
+            if len(f) < 2:
+                continue
+            
+            key, val = f.strip().split('=', 1)
+            if key in glbs:
+                glbs[key] = val.strip('"')
+                
+    else:
+        print("~/.ptutils.config not found")
+        sys.exit(1)
 
 async def torrent2mag(local_fn):
     req_headers = requests.utils.default_headers()
@@ -48,46 +59,49 @@ async def torrent2mag(local_fn):
     return await mag2cloud(magurl)
     
 async def mag2cloud(mag):
+    _re_name = re.compile(r';dn=(\S+)$')
+    fn = _re_name.search(mag)
+    if fn:
+        fn = urllib.parse.unquote_plus(fn[1])
+        print("-->> ", fn, )
+
     loop = asyncio.get_event_loop()
     req_headers = requests.utils.default_headers()
     req_headers.update({
-            'cookie':CLDCOOKIE[8:],
+            'Cookie':CLDCOOKIE[8:],
             'Content-Type': 'application/json'
     })
     url = CLDTORRENT+"/api/magnet"
     r = await loop.run_in_executor(None, lambda :requests.post(url, headers=req_headers, data=mag))
+
     assert r.status_code == 200
-    print(r.text)
+    print(r.text, fn)
     return r.text
 
 async def main():
-
-    conf = os.path.expanduser("~/.ptutils.config")
-    if os.path.exists(conf):
-        with open(conf) as f:
-            configs = f.readlines()
-        
-        glbs = globals()
-        for f in configs:
-            if len(f) < 2:
-                continue
-            
-            key ,val = f.strip().split('=', 1)
-            if key in glbs:
-                glbs[key] = val.strip('"')
-                
-    else:
-        print("~/.ptutils.config not found")
-        sys.exit(1)
-
     futures = [
         asyncio.ensure_future(torrent2mag(local_fn))
         for local_fn in glob.glob(torrent_local_path)
     ]
     print("==="*20)
-    await asyncio.gather(*futures)
+    await asyncio.wait(futures)
+    print("==="*20)
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v', action='count', dest='verbose', help='verbose request process')
+    results = parser.parse_args()
+
+    if results.verbose and results.verbose > 0:
+        logging.basicConfig()
+        logging.getLogger().setLevel(logging.DEBUG)
+        requests_log = logging.getLogger("requests.packages.urllib3")
+        requests_log.setLevel(logging.DEBUG)
+    
+    if results.verbose and results.verbose > 1:
+        HTTPConnection.debuglevel = 1
+
+    readconf()
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
 
