6d6a131215a93531951b005b1719610c12a715df
boypt <1033514+boypt@users.noreply.github.com>
2019-06-25 13:55:10 +0800

    simplify torrentDecode

--- go/torrent2cloud/main.go
+++ go/torrent2cloud/main.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"bufio"
-	"crypto/sha1"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"net/url"
 	"os"
 	"os/user"
 	"path"
@@ -16,66 +14,29 @@ import (
 	"strings"
 	"sync"
 
-	"github.com/anacrolix/torrent/bencode"
+	"github.com/anacrolix/torrent/metainfo"
 	"github.com/joho/godotenv"
 )
 
-type torrentFile struct {
-	Info struct {
-		Name        string `bencode:"name"`
-		Length      int64  `bencode:"length"`
-		MD5Sum      string `bencode:"md5sum,omitempty"`
-		PieceLength int64  `bencode:"piece length"`
-		Pieces      string `bencode:"pieces"`
-		Private     bool   `bencode:"private,omitempty"`
-	} `bencode:"info"`
+func torrent2Magnet(fn string) (*metainfo.Magnet, error) {
 
-	Announce     string      `bencode:"announce"`
-	AnnounceList [][]string  `bencode:"announce-list,omitempty"`
-	CreationDate int64       `bencode:"creation date,omitempty"`
-	Comment      string      `bencode:"comment,omitempty"`
-	CreatedBy    string      `bencode:"created by,omitempty"`
-	URLList      interface{} `bencode:"url-list,omitempty"`
-}
-
-type magnetInfo struct {
-	Name    string
-	MagLink string
-}
-
-func torrentDecode(fn string) (*magnetInfo, error) {
-	var f torrentFile
-	content, err := ioutil.ReadFile(fn)
+	mi, err := metainfo.LoadFromFile(fn)
 	if err != nil {
 		return nil, err
 	}
 
-	//decode
-	err = bencode.Unmarshal([]byte(content), &f)
+	info, err := mi.UnmarshalInfo()
 	if err != nil {
 		return nil, err
 	}
 
-	metainfo, err := bencode.Marshal(f.Info)
-	if err != nil {
-		return nil, err
-	}
-
-	maglink := fmt.Sprintf(
-		"magnet:?xt=urn:btih:%x&dn=%s",
-		sha1.Sum(metainfo),
-		url.QueryEscape(f.Info.Name),
-	)
-
-	return &magnetInfo{
-		Name:    f.Info.Name,
-		MagLink: maglink,
-	}, nil
+	m := mi.Magnet(info.Name, mi.HashInfoBytes())
+	return &m, nil
 }
 
-func mag2cloud(maglink string) (string, error) {
+func mag2cloud(m *metainfo.Magnet) (string, error) {
 	magapi := fmt.Sprintf("%s/api/magnet", os.Getenv("CLDTORRENT"))
-	req, err := http.NewRequest("POST", magapi, strings.NewReader(maglink))
+	req, err := http.NewRequest("POST", magapi, strings.NewReader(m.String()))
 	if err != nil {
 		return "", err
 	}
@@ -117,22 +78,22 @@ func main() {
 
 	for i, torf := range tors {
 		_, base := path.Split(torf)
-		m, err := torrentDecode(torf)
+		m, err := torrent2Magnet(torf)
 		if err != nil {
 			fmt.Println(err)
 			continue
 		}
-		fmt.Printf("[%d] %s --> %s\n%s\n\n", i+1, base, m.Name, m.MagLink)
-		resInfo := fmt.Sprintf("[%d]%s:", i+1, m.Name)
+		fmt.Printf("(%d)%s -> %s\n%s\n\n", i+1, base, m.DisplayName, m.String())
+		resInfo := fmt.Sprintf("(%d)%s:", i+1, m.DisplayName)
 
 		wg.Add(1)
 		go func() {
-			ret, err := mag2cloud(m.MagLink)
+			defer wg.Done()
+			ret, err := mag2cloud(m)
 			if err != nil {
 				ret = err.Error()
 			}
 			resCh <- resInfo + ret
-			wg.Done()
 		}()
 	}
 
