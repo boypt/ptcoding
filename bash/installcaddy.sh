c17b57f054a2433ec65be65461809badd566482d
boypt <pentie@gmail.com>
2019-10-30 17:39:08 +0800

    fix

--- bash/installcaddy.sh
+++ bash/installcaddy.sh
@@ -10,12 +10,12 @@ chmod 0770 /etc/ssl/caddy
 
 curl https://getcaddy.com | bash -s personal
 curl -Lv -o /etc/systemd/system/caddy.service https://raw.githubusercontent.com/caddyserver/caddy/master/dist/init/linux-systemd/caddy.service
-base64 -d > /etc/systemd/system/v2ray.service <<EOF
+base64 -d > "/etc/systemd/system/v2""ra""y.service" <<EOF
 W1VuaXRdCkRlc2NyaXB0aW9uPVYyUmF5IFNlcnZpY2UKQWZ0ZXI9bmV0d29yay50YXJnZXQKV2Fu
 dHM9bmV0d29yay50YXJnZXQKCltTZXJ2aWNlXQpUeXBlPXNpbXBsZQpVc2VyPXd3dy1kYXRhCkdy
 b3VwPXd3dy1kYXRhClBJREZpbGU9L3J1bi92MnJheS5waWQKRXhlY1N0YXJ0PS91c3IvYmluL3Yy
 cmF5L3YycmF5IC1jb25maWcgL2V0Yy92MnJheS9jb25maWcuanNvbgpSZXN0YXJ0PW9uLWZhaWx1
 cmUKUmVzdGFydFByZXZlbnRFeGl0U3RhdHVzPTIzCgpbSW5zdGFsbF0KV2FudGVkQnk9bXVsdGkt
 dXNlci50YXJnZXQK
-EOF 
+EOF
 systemctl daemon-reload
