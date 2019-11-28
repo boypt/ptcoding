87b23bc925e8ff0da2f749b9a8e549e786e935d2
boypt <1033514+boypt@users.noreply.github.com>
2019-11-28 10:16:53 +0800

    use gs3

--- bash/recm3u8.sh
+++ bash/recm3u8.sh
@@ -25,6 +25,8 @@ SRCS["gs"]="http://satellitepull.cnr.cn/live/wxgdgsgb/playlist.m3u8"
 SRCS["zj"]="http://satellitepull.cnr.cn/live/wxgdzjjjt/playlist.m3u8"
 SRCS["gs2"]="http://ls.qingting.fm/live/4847/64k.m3u8?deviceid=00000000-2a50-2e6a-7cbf-0d991c12983e"
 SRCS["zj2"]="http://ls.qingting.fm/live/1259/64k.m3u8?deviceid=00000000-2a50-2e6a-7cbf-0d991c12983e"
+SRCS["gs3"]="http://ctt5.rgd.com.cn/fm953"
+SRCS["zj3"]="http://ctt5.rgd.com.cn/fm974"
 
 SRCINPUT=${SRCS[$SKEY]}
 if [[ -z $SRCINPUT ]]; then  exit 1; fi
