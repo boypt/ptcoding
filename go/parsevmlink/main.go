d40a6accb18b1f2b28fab6975d2d85fb502cdd5d
boypt <1033514+boypt@users.noreply.github.com>
2019-12-20 17:02:03 +0800

    validate

--- go/parsevmlink/main.go
+++ go/parsevmlink/main.go
@@ -6,21 +6,76 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
 	"regexp"
 	"sort"
+	"sync"
 
 	"github.com/mmcdole/gofeed"
 )
 
 var (
-	feedurl string
-	output  string
+	feedurl  string
+	output   string
+	validate bool
 )
+
+func runVmessPing(vmess []string) []string {
+
+	var good []string
+	goodch := make(chan string)
+	var w sync.WaitGroup
+
+	for _, v := range vmess {
+		w.Add(1)
+		go func(lnk string) {
+			cmd := exec.Command("vmessping", "-c", "3", lnk)
+			cmd.Run()
+			if cmd.ProcessState.ExitCode() == 0 {
+				goodch <- lnk
+			}
+			w.Done()
+		}(v)
+	}
+
+	go func() {
+		w.Wait()
+		close(goodch)
+	}()
+
+	for v := range goodch {
+		log.Println("goodlink: ", v)
+		good = append(good, v)
+	}
+
+	return good
+}
+
+func writeLink(vmess []string) {
+	var out io.WriteCloser
+	if output != "" {
+		f, err := os.Create(output)
+		if err != nil {
+			log.Fatal(err)
+		}
+		out = f
+	} else {
+		out = os.Stdout
+	}
+
+	for _, v := range vmess {
+		out.Write([]byte(v))
+		out.Write([]byte("\n"))
+	}
+	out.Write([]byte("\n"))
+	out.Close()
+}
 
 func main() {
 
 	flag.StringVar(&feedurl, "f", "", "feed")
 	flag.StringVar(&output, "o", "", "output")
+	flag.BoolVar(&validate, "v", false, "validate available")
 	flag.Parse()
 
 	fp := gofeed.NewParser()
@@ -40,23 +95,17 @@ func main() {
 		return feed.Items[i].PublishedParsed.After(*(feed.Items[j].PublishedParsed))
 	})
 
-	var out io.WriteCloser
-	if output != "" {
-		f, err := os.Create(output)
-		if err != nil {
-			log.Fatal(err)
-		}
-		out = f
-	} else {
-		out = os.Stdout
-	}
-
+	var vmess []string
 	for _, item := range feed.Items {
 		for _, link := range vmr.FindAllString(item.Description, -1) {
-			out.Write([]byte(link))
-			out.Write([]byte("\n"))
+			vmess = append(vmess, link)
 		}
 	}
-	out.Write([]byte("\n"))
-	out.Close()
+
+	if validate {
+		goodv := runVmessPing(vmess)
+		writeLink(goodv)
+	} else {
+		writeLink(vmess)
+	}
 }
